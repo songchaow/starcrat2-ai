a3bd5e8f36c17a53e1bf2a59cd00d0da7952740a
Songchao Wang <songchaow@outlook.com>
2018-10-30 09:36:32 +0800

    Optimize by using reference instead of copying returning value.

--- src/Util.cpp
+++ src/Util.cpp
@@ -558,7 +558,7 @@ float Util::getThreatRange(const sc2::Unit * unit, const sc2::Unit * threat, CCB
 	const float HARASS_THREAT_MIN_HEIGHT_DIFF = 2.f;
 	const float HARASS_THREAT_RANGE_BUFFER = 1.f;
 	const float HARASS_THREAT_RANGE_HEIGHT_BONUS = 4.f;
-	sc2::GameInfo gameInfo = m_bot.Observation()->GetGameInfo();
+	const sc2::GameInfo &gameInfo = m_bot.Observation()->GetGameInfo();
 	float heightBonus = Util::TerainHeight(gameInfo, threat->pos) > Util::TerainHeight(gameInfo, unit->pos) + HARASS_THREAT_MIN_HEIGHT_DIFF ? HARASS_THREAT_RANGE_HEIGHT_BONUS : 0.f;
 	float threatRange = Util::GetAttackRangeForTarget(threat, unit, m_bot) + Util::getSpeedOfUnit(threat, m_bot) + heightBonus + HARASS_THREAT_RANGE_BUFFER;
 	return threatRange;
