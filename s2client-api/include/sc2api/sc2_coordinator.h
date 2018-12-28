06a93885b159e8f215d423addac2757075afba2e
Songchao Wang <songchaow@outlook.com>
2018-12-28 13:10:38 +0000

    Enable remote execution

--- s2client-api/include/sc2api/sc2_coordinator.h
+++ s2client-api/include/sc2api/sc2_coordinator.h
@@ -111,6 +111,9 @@ public:
     //! Uses settings gathered from LoadSettings, specifically the path to the executable, to run StarCraft II.
     void LaunchStarcraft();
 
+    //! Uses settings gathered from LoadSettings, specifically the path to the executable, to run StarCraft II remotely.
+    void RemoteLaunchStarcraft(std::string ip_addr, int comm_port);
+
     //! Attaches to a running Starcraft.
     void Connect(int port);
 
